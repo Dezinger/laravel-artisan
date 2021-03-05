5528ad80ac2c7ae8b5c33684952a29d1f021560f
Aleksey Kazakov <dezinger@gmail.com>
2021-03-05 15:47:48 +0300

    Configure cron user

--- recipes/schedule_run.rb
+++ recipes/schedule_run.rb
@@ -7,5 +7,6 @@
 ### Add a crontab entry to run laravel artisan scheduler
 artisan_schedule 'schedule:run >> /dev/null 2>&1' do
 	path node['laravel-artisan']['path']
+	user node['laravel-artisan']['queue']['user']
 	action :create
 end
